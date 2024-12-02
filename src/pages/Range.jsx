4fdd8ef1d9ad54a1f700e006ccdcb53be406a6d0
revyrob <kayle.robson@gmail.com>
2024-12-02 12:45:56 -0800

    working on adding signs

--- src/pages/Range.jsx
+++ src/pages/Range.jsx
@@ -6,8 +6,11 @@ import Btn from "../components/Btn/Btn";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header/Header";
 
+//take the sign and the level in the browser to save it there and then carry it to the other comps when needed
+//i can also use the browser to record the score everyday and log it, like Wordle final page
 function Range() {
   const [level, setLevel] = useState("");
+  const [sign, setSign] = useState("");
   const navigate = useNavigate();
 
   //function navigate to equation page
@@ -15,10 +18,17 @@ function Range() {
   function goToEquation(level) {
     navigate(`/equation?level=${level}`);
   }
+
   //onClick for level
   function chooseLevel(e) {
     const value = e.target.value;
     setLevel(value);
+  }
+
+  //onClick for sign
+  function chooseSign(e) {
+    const value = e.target.value;
+    setSign(value);
   }
 
   // Use useEffect to trigger goToEquation after level state has been updated
@@ -33,26 +43,41 @@ function Range() {
         <Header />
         <div className="wizard-style__wrapper">
           <p className="number">Choose your</p>
-          <h1 className="wizard-style__title">Level</h1>
-          <Btn
-            className={"goToGame__btn"}
-            value={"level1"}
-            onClick={chooseLevel}
-            text={"Level 1"}
-          />
+          {sign === "" ? (
+            <>
+              <h1 className="wizard-style__title">Sign</h1>
+              <Btn
+                className={"goToGame__btn"}
+                value={"sign+"}
+                onClick={chooseSign}
+                text={"+"}
+              />
 
-          <Btn
-            className={"goToGame__btn"}
-            value={"level2"}
-            onClick={chooseLevel}
-            text="Level 2"
-          />
-          {/* <Btn
-            className={"goToGame__btn--2"}
-            value={"level3"}
-            onClick={chooseLevel}
-            text="Vivian 3"
-          /> */}
+              <Btn
+                className={"goToGame__btn"}
+                value={"sign*"}
+                onClick={chooseSign}
+                text="x"
+              />
+            </>
+          ) : (
+            <>
+              <h1 className="wizard-style__title">Level</h1>
+              <Btn
+                className={"goToGame__btn"}
+                value={"level1"}
+                onClick={chooseLevel}
+                text={"Level 1"}
+              />
+
+              <Btn
+                className={"goToGame__btn"}
+                value={"level2"}
+                onClick={chooseLevel}
+                text="Level 2"
+              />
+            </>
+          )}
         </div>
       </div>
     </>
